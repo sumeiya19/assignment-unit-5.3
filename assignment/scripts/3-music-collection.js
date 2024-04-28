fb8f0edfc73ba6c6d3d95f4e118b0593318ae7a0
Sumeiya Abdi <sumeiya619@gmail.com>
2024-04-27 21:59:47 -0500

    completed all tests

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,10 +1,48 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+let myCollection = [];
 
+function addToCollection(collection, title, artist, yearPublished){
+  // Create new object with parameters
+  let newAlbum = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  };
+  // Add new object to the end of the collection array
+  collection.push(newAlbum);
+  // Return new object
+  return newAlbum;
+}
+// Use and test addCollection function
+addToCollection(myCollection, "New Blue Sun", "Andre 3000", 2023);
+addToCollection(myCollection, "SZA", "SOS", 2023);
+addToCollection(myCollection, "Renaissance", "Beyonce", 2022 );
+addToCollection(myCollection,"Un Verano Sin Ti", "Bad Bunny", 2022 );
+addToCollection(myCollection,"Call Me If You Get Lost", "Tyler, the Creator", 2021 );
+addToCollection(myCollection, "Sour", "Olivia Rodrigo", 2021);
 
+// Testing addCollection
+console.log('myCollection array:', myCollection); 
 
+function showCollection(collection) {
+  for (let i=0; i<collection.length; i++){
+    let album = collection[i];
+    console.log(album.title + " by " + album.artist + " publihed in " + album.yearPublished);
+  }
+  
+}
 
+function findByArtist(collection, artist){
+let matchingResults = [];
+for (let i=0; i<collection.length; i++){
+  if(collection[i].artist === artist){
+    matchingResults.push(collection[i]);
+  }
+}
+return matchingResults;
+}
 
 
 
